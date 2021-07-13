ed090ac256992270a3f04a3ab5b86bdbf44cf76c
Bhagyashree <bhagya099@gmail.com>
2021-07-13 13:09:03 +1000

    made asimple page I hope you like it

---
+++ src/Category.js
@@ -0,0 +1,60 @@
+
+import React from 'react';
+
+export default function Category() {
+    return (
+    <div className="table_div">
+            <div className="first_cate">
+                <h4>Category 1</h4>
+            <table >
+                <tr>
+                        <th>Items</th>
+                        <th>Value</th>
+                </tr>
+                <tr>
+                        <td>Item1</td>
+                        <td>Value 1</td>
+                    </tr>
+                    <tr>
+                        <td>Item 2</td>
+                        <td>Value 2</td>
+                    </tr>
+                    <tr>
+                        <td>Item 3</td>
+                        <td>Value 3</td>
+                    </tr>
+                    <tr>
+                        <td>Item4</td>
+                        <td>Value 4</td>
+                </tr>
+                    
+            </table>
+        </div>
+        <div className="second_cate">
+                <h4>Category 2</h4>
+                <table >
+                <tr>
+                        <th>Items</th>
+                        <th>Value</th>
+                    </tr>
+                    <tr>
+                        <td>Item1</td>
+                        <td>Value 1</td>
+                    </tr>
+                    <tr>
+                        <td>Item 2</td>
+                        <td>Value 2</td>
+                    </tr>
+                    <tr>
+                        <td>Item 3</td>
+                        <td>Value 3</td>
+                    </tr>
+                    <tr>
+                        <td>Item4</td>
+                        <td>Value 4</td>
+                </tr>
+            </table>
+       </div>    
+    </div>
+)
+}
---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer>copyright &copy; ot his company   </footer>
+    )
+}
