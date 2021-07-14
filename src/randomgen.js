d192dbf4d40e764bf23469cdf108839179723bd9
meng <loh.yumeng@gmail.com>
2021-07-14 16:45:44 +1000

    fixed randomgen cumulative bug

--- src/randomgen.js
+++ src/randomgen.js
@@ -1,28 +1,28 @@
 import { marketCategories } from "./components/cprmarketdata.js";
 
 
-const categoryNames= ['Weapons', 'Foods and Drugs', 'Cyberware', 'Electronics', 'Clothes', 'Surival'];
-
-let randomCats = [];
-let sixCats = [1,2,3,4,5,6];
-let randomtemp = [];
+const categoryNames = ['Weapons', 'Foods and Drugs', 'Cyberware', 'Electronics', 'Clothes', 'Surival'];
 
 export const categorySelector = (quantity = 2) => {
-//  .. randomCats becomes an array of 6 numbers in random order.
-    for (let i=0; i<quantity;i++){
-        let index = sixCats[(Math.floor(Math.random() * sixCats.length))];
-        sixCats.splice(sixCats.indexOf(index), 1);
-        randomtemp.push(index);
-     
-        let randomcat = {
-                id: index-1,
-                categoryTitle: categoryNames[index-1],
-                list: marketCategories[index-1],    
-            }
-        randomCats.push(randomcat);
-    
+  let randomCats = [];
+  let sixCats = [1, 2, 3, 4, 5, 6];
+  let randomtemp = [];
+
+  //  .. randomCats becomes an array of 6 numbers in random order.
+  for (let i = 0; i < quantity; i++) {
+    let index = sixCats[(Math.floor(Math.random() * sixCats.length))];
+    sixCats.splice(sixCats.indexOf(index), 1);
+    randomtemp.push(index);
+
+    let randomcat = {
+      id: index - 1,
+      categoryTitle: categoryNames[index - 1],
+      list: marketCategories[index - 1],
     }
-    return(randomCats);
+    randomCats.push(randomcat);
+
+  }
+  return (randomCats);
 }
 
 console.table(categorySelector());
