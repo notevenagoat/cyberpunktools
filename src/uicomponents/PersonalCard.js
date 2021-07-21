604fe55bcc7974823ddab155599b2e78ee0bda75
meng <loh.yumeng@gmail.com>
2021-07-21 17:00:06 +1000

    built about-us page

---
+++ src/uicomponents/PersonalCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+function PersonalCard(props) {
+  return (
+    <Card bg="dark" style={{ width: '24rem' }} >
+      <Card.Img variant="top" src={props.imageSource} />
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>
+          {props.description}
+        </Card.Text>
+        <Card.Link href={props.link1.url} target="_blank" className="text-white">{props.link1.icon}</Card.Link>
+        <Card.Link href={props.link2.url} target="_blank" className="text-white">{props.link2.icon}</Card.Link>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default PersonalCard
